6a80b0cafaf07cca510dc48569144ba50a3a87b8
david18012003 <bravocarmen264@gmail.com>
2024-06-08 22:48:14 -0500

    creacion de la app del proyecto y todo lo referente a usuarios

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,6 +1,6 @@
 import { createPool } from 'mysql2/promise'
 import dotenv from 'dotenv'
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from '../../config'
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from '../../config.js'
 
 dotenv.config({path:'./src/env/.env'})
 
