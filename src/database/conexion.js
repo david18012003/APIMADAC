9738c4f97d15271f8952c20c688e9a94f63339ab
david18012003 <bravocarmen264@gmail.com>
2024-06-09 17:32:38 -0500

    creacion de la app del proyecto y todo lo referente a usuarios

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -5,9 +5,9 @@ import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from '../../confi
 dotenv.config({path:'./src/env/.env'})
 
 export const pool = createPool({
-    host:DB_HOST, 
-    user:DB_USER,
-    password:DB_PASSWORD,
-    port:DB_PORT,
-    database:DB_DATABASE
+    host:'roundhouse.proxy.rlwy.net', 
+    user:'root',
+    password:'hyMFaEXhckCYJsVeZkBKHQARgtsSiFcP',
+    port:'10092',
+    database:'railway'
 })
