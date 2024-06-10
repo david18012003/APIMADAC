496473ecd2185e679ed7882afdff4b5629790819
david18012003 <bravocarmen264@gmail.com>
2024-06-09 19:17:51 -0500

    actualizacion

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,6 +1,5 @@
 import { createPool } from 'mysql2/promise'
 import dotenv from 'dotenv'
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from '../../config.js'
 
 dotenv.config({path:'./src/env/.env'})
 
