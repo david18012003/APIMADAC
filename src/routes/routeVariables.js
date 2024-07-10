d39ac908fd0a80d5f67f249f9e47b6f00fb2ba9f
david18012003 <bravocarmen264@gmail.com>
2024-07-09 20:24:00 -0500

    se arreglo la ruta de las muestras

--- src/routes/routeVariables.js
+++ src/routes/routeVariables.js
@@ -5,14 +5,14 @@ import { validarToken } from "../controllers/seguridad.controller.js";
 
 const routeVariables = Router()
 
-routeVariables.get("/listarvariable", validarToken, listarVariables)
+routeVariables.get("/listar", validarToken, listarVariables)
 
-routeVariables.post("/crearvariable",/* validarToken, */ /* validacionVariable, */CrearVariable)
+routeVariables.post("/crear",/* validarToken, */ /* validacionVariable, */CrearVariable)
 
-routeVariables.put("/actualizarvariable/:codigo",validarToken, ActualizarVariable)
-routeVariables.put("/desactivarVariable/:codigo",validarToken, desactivarVariable)
-routeVariables.put("/activarVariable/:codigo",validarToken, activarVariable)
-routeVariables.get("/buscarvariable/:codigo",validarToken, buscarvariable)
+routeVariables.put("/actualizar/:codigo",validarToken, ActualizarVariable)
+routeVariables.put("/desactivar/:codigo",validarToken, desactivarVariable)
+routeVariables.put("/activar/:codigo",validarToken, activarVariable)
+routeVariables.get("/buscar/:codigo",validarToken, buscarvariable)
 routeVariables.get("/activas",validarToken, variablesActivas)
 
 
